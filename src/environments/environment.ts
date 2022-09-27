56a31c0157073bd783884f1fb4af18843c2dd5d6
juusoforsm <juuso.forsman@gmail.com>
2022-09-27 13:54:51 +0300

    committing changes this way

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '060a8792f690ee4f8d6c19722f208980', // <-- Enter your own key here!'
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'http://image.tmdb.org/t/p',
 };
 
 /*
